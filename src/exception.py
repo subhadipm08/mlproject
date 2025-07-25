981131403ae93d3570ce80752f224d9f97682b34
subhadipm08 <subhadipmudi2020@gmail.com>
2025-07-25 19:37:10 +0530

    update in exception.py

--- src/exception.py
+++ src/exception.py
@@ -17,9 +17,9 @@ class CustomException(Exception):
         return self.error_message
     
 
-if __name__=="__main__":
-    try:
-        x=1/0
-    except Exception as e:
-        logging.info("Division by Zero")
-        raise CustomException(e,sys)
+# if __name__=="__main__":
+#     try:
+#         x=1/0
+#     except Exception as e:
+#         logging.info("Division by Zero")
+#         raise CustomException(e,sys)
